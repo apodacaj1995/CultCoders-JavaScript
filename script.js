a5cd9754d27e8fcdf8e8df8e13bcfe0203900722
apodacaj1995 <67032944+apodacaj1995@users.noreply.github.com>
2020-07-06 20:15:25 -0600

    initial  commit
    added completed beginner and intermediate exercises

---
+++ script.js
@@ -0,0 +1,105 @@
+// BEGINNER
+let a;
+let b;
+
+a = "Hello";
+b = "World";
+
+function sayingHi (x,y) {
+    
+    return x + "" +y;
+    
+
+}
+console.log(sayingHi (a,b));
+
+
+
+
+
+const palabra = "Javascript"
+function print (palabra){
+    let result = "Javascript";
+    return result;
+
+}
+console.log(print(palabra))
+
+
+
+
+const num1 = 2;
+const num2 = 2;
+function add(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+console.log(add (2,2))
+
+
+
+
+
+let c = 3;
+let d = 0;
+console.log(c>d);
+
+
+
+
+
+var myStr ="Hello,";
+var myOtherStr = "Jackie!";
+console.log(myStr+myOtherStr);
+
+
+
+
+
+function PEMDAS (n) {
+    let result = n + 1 * 2 - 3 / 4;
+    return result;
+}
+console.log(PEMDAS(5))
+
+
+// INTERMEDIATE
+
+function division (n) {
+    let result = n / 7
+    return result;
+}
+console.log(division(14))
+
+
+
+
+const str = "twice";
+const repeated = str.repeat(2);
+console.log(repeated);
+
+
+
+
+const my3Str = "string";
+const repeated1 = my3Str.repeat(4);
+console.log(repeated1);
+
+
+
+
+const user = "Jackie"
+const days = "four"
+const box = `It has been ${days} days since ${user} has logged in.`;
+console.log(box);
+
+
+
+// let p = "jackie";
+// let w = "four";
+// fuction replace (p,w) {
+   
+//     const box1 = `It has been ${w} days since ${p} has logged in`;
+    
+// }
+// console.log(replace);
